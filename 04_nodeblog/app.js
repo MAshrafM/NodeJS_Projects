0ff0a6801988a8c6f1375175880f5ff8965b1209
MAshraf <ee.mashraf@gmail.com>
2016-07-07 18:15:27 +0300

    jade layout and app middle

--- 04_nodeblog/app.js
+++ 04_nodeblog/app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
-
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+var multer = require('multer');
+var flash = require('connect-flash');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -14,14 +19,56 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use( multer({dest: './public/images/uploads'}).single('photo'));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handle session
+app.use(session({
+	secret: 'sonic',
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Validator
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value){
+		var namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+		
+		while(namespace.length){
+			formParam += '[' + namespace.shift() + ']';
+		}
+		
+		return{
+			param: formParam,
+			msg: msg,
+			value: value
+		}
+	}
+}));
+
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(flash());
+app.use(function(req, res, next){
+	res.locals.messages = require('express-messages')(req,res);
+	next();
+});
+
+// access db
+app.use(function(req, res, next){
+	req.db = db;
+	next();
+});
 app.use('/', routes);
 app.use('/users', users);
 
